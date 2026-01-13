73b0c25f9cb55ed473b14fcd9a9b445df4b82173
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-01-13 14:09:35 +0700

    fix: add usePublicPath method to application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -43,4 +43,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create()
+    ->usePublicPath(realpath(__DIR__.'/../'));
