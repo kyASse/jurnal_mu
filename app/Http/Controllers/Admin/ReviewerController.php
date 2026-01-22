5e36aef8f0b9031cc39bde6d399cad2238bb0572
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-01-22 12:57:10 +0700

    feat: add Reviewer management controller and placeholder page

---
+++ app/Http/Controllers/Admin/ReviewerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ReviewerController extends Controller
+{
+    /**
+     * Display a listing of Reviewers.
+     * 
+     * v1.1 feature - Reviewer management placeholder
+     */
+    public function index(): Response
+    {
+        // Only Super Admin can access
+        $this->authorize('manage-users');
+
+        return Inertia::render('Admin/Reviewers/Index');
+    }
+}
