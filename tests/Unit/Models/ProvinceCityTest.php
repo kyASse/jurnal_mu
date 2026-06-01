472547a9732df27cb620b65c275d7433fdcc7328
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-06-01 10:17:20 +0700

    test: update ProvinceCityTest to use integer IDs

--- tests/Unit/Models/ProvinceCityTest.php
+++ tests/Unit/Models/ProvinceCityTest.php
@@ -6,11 +6,26 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
+beforeEach(function () {
+    Province::unguard();
+    City::unguard();
+});
+
+afterEach(function () {
+    Province::reguard();
+    City::reguard();
+});
+
 test('province can be created and has correct fillable attributes', function () {
-    $province = Province::create([
+    Province::reguard();
+
+    $province = new Province();
+    $province->id = 1;
+    $province->fill([
         'name' => 'Jawa Barat',
         'extra_field' => 'should be ignored',
     ]);
+    $province->save();
 
     expect($province)->toBeInstanceOf(Province::class)
         ->and($province->name)->toBe('Jawa Barat')
@@ -18,13 +33,21 @@ test('province can be created and has correct fillable attributes', function () 
 });
 
 test('city can be created and has correct fillable attributes', function () {
-    $province = Province::create(['name' => 'Jawa Barat']);
+    $province = Province::create([
+        'id' => 1,
+        'name' => 'Jawa Barat',
+    ]);
 
-    $city = City::create([
+    City::reguard();
+
+    $city = new City();
+    $city->id = 10;
+    $city->fill([
         'province_id' => $province->id,
         'name' => 'Bandung',
         'extra_field' => 'should be ignored',
     ]);
+    $city->save();
 
     expect($city)->toBeInstanceOf(City::class)
         ->and($city->province_id)->toBe($province->id)
@@ -33,17 +56,30 @@ test('city can be created and has correct fillable attributes', function () {
 });
 
 test('province has many cities relationship', function () {
-    $province = Province::create(['name' => 'Jawa Barat']);
-    $city1 = $province->cities()->create(['name' => 'Bandung']);
-    $city2 = $province->cities()->create(['name' => 'Bekasi']);
+    $province = Province::create([
+        'id' => 1,
+        'name' => 'Jawa Barat',
+    ]);
+    $city1 = $province->cities()->create([
+        'id' => 10,
+        'name' => 'Bandung',
+    ]);
+    $city2 = $province->cities()->create([
+        'id' => 11,
+        'name' => 'Bekasi',
+    ]);
 
     expect($province->cities)->toHaveCount(2)
         ->and($province->cities->pluck('name')->toArray())->toEqualCanonicalizing(['Bandung', 'Bekasi']);
 });
 
 test('city belongs to province relationship', function () {
-    $province = Province::create(['name' => 'Jawa Barat']);
+    $province = Province::create([
+        'id' => 1,
+        'name' => 'Jawa Barat',
+    ]);
     $city = City::create([
+        'id' => 10,
         'province_id' => $province->id,
         'name' => 'Bandung',
     ]);
