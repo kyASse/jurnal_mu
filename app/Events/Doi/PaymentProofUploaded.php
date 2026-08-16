1e6af9ae736fff1c680fcdc7e8b684de68ac4a02
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-08-16 10:28:31 +0700

    feat(doi): configure private storage disk and domain events

---
+++ app/Events/Doi/PaymentProofUploaded.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Events\Doi;
+
+use App\Models\DoiPaymentProof;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PaymentProofUploaded
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public DoiPaymentProof $paymentProof
+    ) {}
+}
